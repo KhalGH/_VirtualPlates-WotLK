a01c681a92a34a2ffbc6eb399dd4f774d4e195be
Khal <111320187+KhalGH@users.noreply.github.com>
2025-05-31 12:05:32 -0500

    Added _VirtualPlates files (modified version)

---
+++ !!!_VirtualPlates/Locales/Locale-esMX.lua
@@ -0,0 +1,45 @@
+--[[****************************************************************************
+  * _VirtualPlates by Saiket                                                   *
+  * Locales/Locale-esMX.lua - Localized string constants (es-MX).              *
+  ****************************************************************************]]
+
+if ( GetLocale() ~= "esMX" ) then
+	return;
+end
+
+if C_NamePlate and C_NamePlate.GetNamePlatesDistance then
+	local _VirtualPlates = select( 2, ... );
+	_VirtualPlates.L = setmetatable( {
+		CONFIG_DESC = 	"Configura la forma en que _VirtualPlates escala los nameplates.\n" ..
+					  	"Aumenta el CVar 'nameplateDistance' para extender el rango de visibilidad (valor actual: " .. GetCVar("nameplateDistance") .. ").\n\n" ..
+					  	"Autor: Saiket\nVersión mejorada por |cffc41f3bKhal|r",
+		CONFIG_LIMITS = "Límites de Escalado de Nameplates",
+		CONFIG_MAXSCALE = "Escala por defecto",
+		CONFIG_MAXSCALEENABLED = "Cambiar el tamaño por defecto",
+		CONFIG_MAXSCALEENABLED_DESC = "Ajusta el tamaño por defecto de los nameplates usando este factor",
+		CONFIG_MINSCALE = "Escala Mínima",
+		CONFIG_MINSCALE_DESC = "Define la escala mínima para nameplates a grandes distancias. La distancia asociada a la escala mínima es el CVar 'nameplateDistance' con un cap de 100 m.",
+		CONFIG_SCALEFACTOR = "Distancia Umbral de Escalado",
+		CONFIG_SCALEFACTOR_DESC = "Los nameplates más cercanos que esta distancia se mostrarán con su tamaño por defecto, y se reducirán gradualmente a medida que aumente la distancia por encima del umbral.",
+		CONFIG_SLIDER_FORMAT = "%.2f",
+		CONFIG_SLIDERYARD_FORMAT = "%dm",
+		CONFIG_TITLE = "_|cffCCCC88VirtualPlates|r",
+	}, { __index = _VirtualPlates.L; } );
+else
+	local _VirtualPlates = select( 2, ... );
+	_VirtualPlates.L = setmetatable( {
+		CONFIG_DESC = "Configura la forma en que _VirtualPlates escala los nameplates.",
+		CONFIG_LIMITS = "Límites de Escalado de Nameplates",
+		CONFIG_MAXSCALE = "Máximo",
+		CONFIG_MAXSCALEENABLED = "Límite de escala máxima",
+		CONFIG_MAXSCALEENABLED_DESC = "Previene que los nameplates se vuelvan muy grandes al estar cerca de la cámara.",
+		CONFIG_MINSCALE = "Mínimo",
+		CONFIG_MINSCALE_DESC = "Limita qué tanto pueden encogerse los nameplates, desde 0 (sin límite) hasta 1 (no podrán ser más pequeños que el tamaño por defecto).",
+		CONFIG_SCALEFACTOR = "Distancia de Normalización de Escala",
+		CONFIG_SCALEFACTOR_DESC = "Los nameplates a esta distancia de la cámara tendrán escala normal",
+		CONFIG_SLIDER_FORMAT = "%.2f",
+		CONFIG_SLIDERYARD_FORMAT = "%dm",
+		CONFIG_TITLE = "_|cffCCCC88VirtualPlates|r",
+	}, { __index = _VirtualPlates.L; } );
+end
+
